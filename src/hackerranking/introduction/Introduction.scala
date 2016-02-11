d3810c45aced63f5f786fa9bed6ed6238c7479c3
helderppaulo <helderppaulo89@gmail.com>
2016-02-11 14:24:35 -0200

    hello world

--- src/hackerranking/introduction/Introduction.scala
+++ src/hackerranking/introduction/Introduction.scala
@@ -6,10 +6,12 @@ package hackerranking.introduction
 object Introduction {
 
   def main(args: Array[String]) {
-    solvemefirst()
+    helloworld()
   }
 
   def solvemefirst() = println(io.Source.stdin.getLines().take(2).map(_.toInt).sum)
+  
+  def helloworld() = println("Hello World")
 
 }
 
