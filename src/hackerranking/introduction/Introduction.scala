2692ea7cb1acd76f5f0fb84da49099c2ede6363b
helderppaulo <helderppaulo89@gmail.com>
2016-02-11 14:42:45 -0200

    Filter positions in a list

--- src/hackerranking/introduction/Introduction.scala
+++ src/hackerranking/introduction/Introduction.scala
@@ -6,16 +6,20 @@ package hackerranking.introduction
 object Introduction {
 
   def main(args: Array[String]) {
-    println(listreplication(4, List(1,2,3)))
+    println(filterpositionsinalist(io.Source.stdin.getLines().map(_.toInt).toList))
   }
 
   def solvemefirst() = println(io.Source.stdin.getLines().take(2).map(_.toInt).sum)
-  
+
   def helloworld() = println("Hello World")
-  
+
   def helloworldNtimes(n: Int) = (1 to n).foreach(_ => helloworld())
-  
+
   def listreplication(n: Int, arr: List[Int]): List[Int] = arr.map(x => (1 to n).map(_ => x)).flatten.toList
+
+  def filterarray(delim: Int, arr: List[Int]): List[Int] = arr.filter(x => x < delim)
+
+  def filterpositionsinalist(arr: List[Int]): List[Int] = arr.zipWithIndex.filter(x => x._2 % 2 == 1).map(x => x._1)
 
 }
 
